99b4efbaa127107e5ce85e39030a26b8277d2e73
WolfieBoy <joshuaxmas@icloud.com>
2021-11-12 21:42:30 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='CaeserPlus',
+    url=https://github.com/awesomelewis2007/CaesarPlus/'',
+    author='Lewis Evans',
+    author_email='awesomelewis2007@gmail.com',
+    packages=['caeserplus'],
+    install_requires=[''],
+    version='0.1.1',
+    license='GNU',
+    description='An advanced caesar cypher python module'
+)
