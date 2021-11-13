860d87ba90cf1b88d44f5e0e35d80e866228e044
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-11-13 13:03:49 +0000

    change the version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='awesomelewis2007@gmail.com',
     packages=['caeserplus'],
     install_requires=[''],
-    version='0.1.2',
+    version='0.1.3',
     license='GNU',
     description='An advanced caesar cypher python module'
 )
