1ba2a102093b9e941c258763a38b12871ecee0c9
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-11-14 15:45:16 +0000

    Add readme to pypi project

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
     name='caesarplus',
     url='https://github.com/awesomelewis2007/CaesarPlus/',
@@ -7,7 +10,9 @@ setup(
     author_email='awesomelewis2007@gmail.com',
     packages=['caesarplus'],
     install_requires=[''],
-    version='0.1.4',
+    version='0.1.5',
     license='GNU',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     description='An advanced caesar cypher python module'
 )
